8e438bbdc29d56be252f0a86d7a69e6a7dc6625d
losecow <jinwoo0712@naver.com>
2024-12-21 21:11:49 +0900

    first push

---
+++ Python/STEP_7/addProcession.py
@@ -0,0 +1,16 @@
+N, M = map(int, input().split())
+
+A = []
+B = []
+
+for _ in range(N):
+    A.append(list(map(int, input().split())))
+
+for _ in range(N):
+    B.append(list(map(int, input().split())))
+
+for i in range(N):
+    for j in range(M):
+        result = A[i][j] + B[i][j]
+        print(result, end=" ")
+    print()
