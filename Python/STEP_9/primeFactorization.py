6c020e5dc2f85f0e66b9b036b9d5836a478ceaa8
losecow <losecow@handong.ac.kr>
2025-01-06 08:22:46 +0900

    sieveoferatosthenes

---
+++ Python/STEP_9/primeFactorization.py
@@ -0,0 +1,25 @@
+def sieve_of_eratosthenes(N):
+    primeNum = [True] * (N + 1)
+    primeNum[0] = primeNum[1] = False  
+
+    for i in range(2, int(N**0.5) + 1):
+        if primeNum[i]:  
+            for j in range(i * i, N + 1, i):  
+                primeNum[j] = False  
+
+    primeNum = [i for i in range(2, N + 1) if primeNum[i]]
+    return primeNum
+
+N = int(input())
+
+primeNum = sieve_of_eratosthenes(N)
+cnt = 0
+
+while True:
+    if N % primeNum[cnt] == 0:
+        print(primeNum[cnt])
+        N = N // primeNum[cnt]
+    else:
+        cnt += 1
+    if N == 1:
+        break
