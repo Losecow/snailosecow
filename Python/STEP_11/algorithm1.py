0e240aceaed58e60234394fbcdc7042c9ac3d17a
losecow <losecow@handong.ac.kr>
2025-01-12 17:27:01 +0900

    real start

---
+++ Python/STEP_11/algorithm1.py
@@ -0,0 +1,9 @@
+# MenOfPassion(A[], n) {
+#     i = ⌊n / 2⌋;
+#     return A[i]; # 코드1
+# }
+
+print(1)
+print(0)
+
+# https://hanamon.kr/%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98-time-complexity-%EC%8B%9C%EA%B0%84-%EB%B3%B5%EC%9E%A1%EB%8F%84/
