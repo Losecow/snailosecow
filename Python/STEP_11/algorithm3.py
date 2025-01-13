4a8fa9a3faf524ff22100e59651f0e66e96b6fa0
losecow <losecow@handong.ac.kr>
2025-01-14 08:35:57 +0900

    asd

---
+++ Python/STEP_11/algorithm3.py
@@ -0,0 +1,12 @@
+# MenOfPassion(A[], n) {
+#     sum <- 0;
+#     for i <- 1 to n
+#         for j <- 1 to n
+#             sum <- sum + A[i] × A[j]; # 코드1
+#     return sum;
+# }
+
+
+N = int(input())
+print(N * N)
+print(2)
