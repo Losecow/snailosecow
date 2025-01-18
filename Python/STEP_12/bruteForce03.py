94f7657eddce799d74ce1cee91f57af13c60d3dd
losecow <100589396+Losecow@users.noreply.github.com>
2025-01-18 16:00:15 +0900

    Create bruteForce03.py

---
+++ Python/STEP_12/bruteForce03.py
@@ -0,0 +1,8 @@
+a, b, c, d, e, f = map(int, input().split())
+
+for i in range(-999, 1000):
+    for j in range(-999, 1000):
+        if (a*i) + (b*j) == c and (d*i) + (e*j) == f:
+            print(i,j)
+
+# 알고리듬 문제는 알고리듬 활용해서 풀 것
