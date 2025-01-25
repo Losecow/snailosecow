8a0523900cea0135b8953fce36a6362efd7ad1ee
losecow <losecow@handong.ac.kr>
2025-01-25 15:31:16 +0900

    solved!

--- Python/STUDY/2822.py
+++ Python/STUDY/2822.py
@@ -4,16 +4,11 @@ for i in range(1, 9):
     x = int(input())
     point[x] = i
 
-print(point)
-
 pointSort = sorted(point.items(), reverse = True)
-# point1 = sorted(point, reverse = True)
 
-print(point1)
+sumPoint = sum(P[0] for P in pointSort[:5])
+listPoint = [P[1] for P in pointSort[:5]]
+listPoint.sort()
 
-sumPoint = 0
-temp = 0
-cnt = 0
-
-for i in range(5):
-    sumPoint += pointSort
+print(sumPoint)
+print(*listPoint)
