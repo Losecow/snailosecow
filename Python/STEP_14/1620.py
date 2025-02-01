5d29be6b070bb8302c749bf9e0dbe128a0b68904
losecow <losecow@handong.ac.kr>
2025-02-01 09:02:53 +0900

    asdf

---
+++ Python/STEP_14/1620.py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+
+poketmonName = {}
+poketmonNum = {}
+
+# 개천재발상  -> 딕셔너리에 이름:번호, 번호:이름 두가지를 저장하면 되잖아???
+
+for i in range(1, N + 1):
+    x = input().rstrip()
+    poketmonNum[i] = x
+    poketmonName[x] = i
+
+for i in range(M):
+    x = input().rstrip()
+    if x.isdigit():
+        print(poketmonNum[int(x)])
+    else:
+        print(poketmonName[x])
+
+# 출력값이 조금 이상한데 나중에확인인
