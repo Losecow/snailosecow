72ca78658de63ebbb69336f6a594db8faeaf9922
Losecow <100589396+Losecow@users.noreply.github.com>
2025-07-23 10:15:24 +0900

    week5

---
+++ PPS/week_5/A043_박진우_20250722.py
@@ -0,0 +1,13 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if not strs:
+            return ""
+
+        prefix = strs[0]
+
+        for word in strs[1:]:
+            while not word.startswith(prefix):
+                prefix = prefix[:-1]
+                if prefix == "":
+                    return ""
+        return prefix
