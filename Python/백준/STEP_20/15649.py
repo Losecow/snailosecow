609af23e90cd7dc8c6e3c6766ccdec2fe16e835f
losecow <jinwoo0712@naver.com>
2025-12-24 23:52:05 +0900

    1224

--- Python/백준/STEP_20/15649.py
+++ Python/백준/STEP_20/15649.py
@@ -1,18 +1,40 @@
+# import sys
+# input = sys.stdin.readline
+
+# def backtracking():
+#     if len(array) == m:
+#         print(" ".join(map(str, array)))
+#         return
+
+#     for i in range(1, n+1):
+#         if i not in array:
+#             array.append(i)
+#             backtracking()
+#             array.pop()
+
+# n, m = map(int,input().split())
+# array = []
+
+# backtracking()
+
 import sys
 input = sys.stdin.readline
-
-def backtracking():
-    if len(array) == m:
-        print(" ".join(map(str, array)))
-        return
-
-    for i in range(1, n+1):
-        if i not in array:
-            array.append(i)
-            backtracking()
-            array.pop()
 
 n, m = map(int,input().split())
 array = []
 
-backtracking()
+
+def backtracking(start):
+    
+    if len(array) == m:
+        print(" ".join(map(str, array)))
+        return
+
+
+    for i in range(start, n + 1):
+        array.append(i)
+        start = i
+        backtracking(start)
+        array.pop()
+
+backtracking(n)
