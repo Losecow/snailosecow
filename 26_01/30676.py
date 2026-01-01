6cecd50c393223508c9c904f52c72aee710df73a
losecow <jinwoo0712@naver.com>
2026-01-01 21:51:25 +0900

    0101

---
+++ 26_01/30676.py
@@ -0,0 +1,16 @@
+n = int(input())
+
+if n <= 780 and n >= 620:
+    print("Red")
+elif n < 620 and n >= 590:
+    print("Orange")
+elif n < 590 and n >= 570:
+    print("Yellow")
+elif n < 570 and n >= 495:
+    print("Green")
+elif n < 495 and n >= 450:
+    print("Blue")
+elif n < 450 and n >= 425:
+    print("Indigo")
+elif n < 425 and n >= 380:
+    print("Violet")
