7da98cc5da31d65c861ce59319b147cfaf4cd805
losecow <jinwoo0712@naver.com>
2026-01-03 14:39:05 +0900

    0103

---
+++ Python/백준/STEP_20/15652.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int,input().split())
+array = []
+
+
+def backtracking(start):
+    
+    if len(array) == m:
+        print(" ".join(map(str, array)))
+        return
+
+    for i in range(start, n + 1):
+        array.append(i)
+        backtracking(i)
+        array.pop()
+
+backtracking(1)
