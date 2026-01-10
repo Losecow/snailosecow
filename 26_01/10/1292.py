f1db72446beb1ba40721de35a494dc537460b1b8
losecow <jinwoo0712@naver.com>
2026-01-10 23:49:22 +0900

    0110

--- 26_01/10/1292.py
+++ 26_01/10/1292.py
@@ -15,3 +15,23 @@
     
 # print(arr[m - 1] - arr[n - 2]) 여기가 틀림 (n == 1 일때 고려 안함)
 # # print(arr)
+
+
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+arr = []
+num = 1
+
+while len(arr) < m:
+    for _ in range(num):
+        arr.append(num)
+    num += 1
+
+prefix = [0]
+for x in arr:
+    prefix.append(prefix[-1] + x)
+
+print(prefix[m] - prefix[n-1])
