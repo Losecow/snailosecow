a007647515b025cb6799e5abc66b89745bf72c0f
losecow <jinwoo0712@naver.com>
2026-01-15 23:53:52 +0900

    0115

--- 26_01/15/11279.py
+++ 26_01/15/11279.py
@@ -1,31 +1,53 @@
+# import sys
+# input = sys.stdin.readline
+
+# n = int(input())
+
+# arr = []
+
+# for i in range(n):
+#     m = int(input())
+#     arr.sort()
+
+#     if len(arr) == 0:
+#         if m != 0:
+#             arr.append(m)
+#         else:
+#             print("0")
+
+#     else:
+#         if m == 0:
+#             print(arr.pop())
+
+#         else:
+#             temp = arr.pop()
+
+#             if temp < m:
+#                 arr.append(temp)
+#                 arr.append(m)
+#             else:
+#                 arr.append(m)
+#                 arr.append(temp)
+
+
 import sys
 input = sys.stdin.readline
 
 n = int(input())
-
 arr = []
 
-for i in range(n):
+for _ in range(n):
     m = int(input())
-    arr.sort()
 
-    if len(arr) == 0:
-        if m != 0:
-            arr.append(m)
+    if m == 0:
+        if arr:
+            print(arr.pop())   
         else:
             print("0")
-
     else:
-        if m == 0:
-            print(arr.pop())
+        i = len(arr) - 1
+        while i >= 0 and arr[i] > m:
+            i -= 1
+        arr.insert(i + 1, m)
 
-        else:
-            temp = arr.pop()
-
-            if temp < m:
-                arr.append(temp)
-                arr.append(m)
-            else:
-                arr.append(m)
-                arr.append(temp)
 
