2c6780beecf1f72925c9bae351440f65811e9476
losecow <jinwoo0712@naver.com>
2026-01-31 14:54:28 +0900

    0131

---
+++ 26_01/31/30402.py
@@ -0,0 +1,12 @@
+for _ in range(15):
+    line = input()
+    
+    if 'w' in line:
+        print("chunbae")
+        break
+    elif 'b' in line:
+        print("nabi")
+        break
+    elif 'g' in line:
+        print("yeongcheol")
+        break
