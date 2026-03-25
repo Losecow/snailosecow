c337635d39e9cb639570f2db80bbc65360689ae2
losecow <losecow@handong.ac.kr>
2026-03-25 23:41:42 +0900

    0325

---
+++ C/a_3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    int fat;
+
+    scanf("%d", &a);
+  
+    for(int i = 0; i < a; i++) {
+      int height, weight;   
+      float bmi;          
+
+      scanf("%d %d", &height, &weight);
+
+      bmi = weight / ((height / 100.0) * (height / 100.0));
+
+      if(bmi >= 25) {
+        fat += 1;
+      }
+    }
+
+    printf("%d", fat);
+    
+    return 0;
+}
